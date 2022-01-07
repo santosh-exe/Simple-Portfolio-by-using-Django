a78315ca54051a37c89e995c5f608ea0b381e6d6
santosh-exe <96971356+santosh-exe@users.noreply.github.com>
2022-01-07 15:55:29 +0545

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -153,8 +153,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com.'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'santosh.kh015@gmail.com'
-EMAIL_HOST_PASSWORD = 'santosh@acd'
+EMAIL_HOST_USER = '' #Enter your email address here
+EMAIL_HOST_PASSWORD = '' #Enter your password here
 
 # for message
 from django.contrib.messages import constants as messages
